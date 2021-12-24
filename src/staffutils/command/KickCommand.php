e56533279718eddd9737d2a5a9f50989e4c4e829
Xavier <xavier04.martinez@gmail.com>
2021-12-23 22:11:13 -0600

    kick command added

--- src/staffutils/command/KickCommand.php
+++ src/staffutils/command/KickCommand.php
@@ -4,6 +4,35 @@ declare(strict_types=1);
 
 namespace staffutils\command;
 
-class KickCommand {
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\utils\CommandException;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+use staffutils\StaffUtils;
 
+class KickCommand extends Command {
+
+    /**
+     * @param CommandSender $sender
+     * @param string        $commandLabel
+     * @param array         $args
+     */
+    public function execute(CommandSender $sender, string $commandLabel, array $args): void {
+        if (($name = array_shift($args)) === null) {
+            $sender->sendMessage(TextFormat::RED . 'Usage: /' . $commandLabel . ' <player> <?reason>');
+
+            return;
+        }
+
+        if (($target = Server::getInstance()->getPlayerByPrefix($name)) === null) {
+            $sender->sendMessage(StaffUtils::replacePlaceholders('PLAYER_NOT_FOUND', $name));
+
+            return;
+        }
+
+        $target->kick(StaffUtils::replacePlaceholders('PLAYER_KICK', $sender->getName(), implode(' ', $args)));
+
+        Server::getInstance()->broadcastMessage(StaffUtils::replacePlaceholders('PLAYER_KICKED', $target->getName(), $sender->getName(), implode(' ', $args)));
+    }
 }
